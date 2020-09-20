b312b3c51a30f2bca33f333b043962206a90a909
hugo matos <hugocfmatos@gmail.com>
2020-09-20 16:16:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (let i=0;i<array.length; i++) {
     if (array[i]===findFn()){
-      return "omg"
+      return array[i]
     }
   }
   return null
